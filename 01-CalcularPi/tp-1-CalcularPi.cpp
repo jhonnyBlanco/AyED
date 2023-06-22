0f513ef48b102f090f0e2cbe5490b8934677cb35
jhonnyblanco <blancojhonny2001@gmail.com>
2023-06-22 20:32:40 -0300

    cambios

--- 01-CalcularPi/tp-1-CalcularPi.cpp
+++ 01-CalcularPi/tp-1-CalcularPi.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <iomanip>
-#include <math.h>
+ 
 
 using namespace std;
 
 int main()
 {
-    int i=0;
-    double pi=0;
-    double resultado=0;
-
-    for (i=0 ; i<=1999999 ; i++)
+    double pi=0;                    /*almacenas el resultado total de todas las iteraciones*/
+    double error = 0.0000001/4;     /*para que se detenga las iteraciones al llegar Aa los 7 decimales*/
+    double termino =0;              /*el resultado cada vez que hace la iteracion*/
+    double signo = 1;               /*para poder hacer los calculos cuando los resaldos sean negativos o positivos*/
+    int n=0;
+   
+    while (abs(termino) >= error || n==0) /*detenerse cuando llegue al numero de decimales que quiero y que aun que n=0 continue iterando*/
     {
-        resultado = pow(-1,i)/(2*i+1);
-        pi += resultado;
-    }
-    
-    pi*=4;
+        
+        termino=signo/(2 * n + 1);      /*los calculos de cada iteracion usando la serie de Liebniz*/
+        pi += termino;                  /*pi va almacenendo los resultados de cada iteracion*/
 
-    cout << " el valor de pi es: "<< setprecision(7) << pi << endl;
+        signo *=(-1);                   /*para tener en cuenta si es negatiuvo o positivo*/
+        n++;                            
+    }  
+    pi *= 4;
+    
+    
+    cout<< setprecision(7)<<fixed <<"El valor de pi es: "<< pi << endl;
+    /*el resultado esta con 7 decimales porque al usar el setprecision 
+    con 6 decimales lo redondea*/
+
 
     return 0;
 }
